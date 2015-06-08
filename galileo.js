98f1ede706fef9446e3d60cefbddd72c345aad82
Charlier Cyril <charlier.cy@gmail.com>
2015-06-08 14:07:05 +0200

    script auto sketch

--- galileo.js
+++ galileo.js
@@ -1,26 +1,36 @@
 var clientio  = require('socket.io-client');
 var client    = clientio.connect('http://194.57.110.4:80');
+var myArgs = process.argv.slice(3);
 client.on('identifiant',function(data){
         console.log('Vous avez cet identifiant : '+ data);
 });
 client.emit('client', 'galileo');
-function Envoi() {
-         a =Math.floor(Math.random()*11) ,z=Math.floor(Math.random()*11)
-        for(var i=1;i<=10;i++){
-            xi=Math.floor(Math.random()*11), yi =Math.floor(Math.random()*11);
+function Envoi(){
+        x = myArgs[0];
+        y = myArgs[1];
+        // x =Math.floor(Math.random()*11) ,y=Math.floor(Math.random()*11)
+        //for(var i=1;i<=10;i++){
+            //xi=Math.floor(Math.random()*11), yi =Math.floor(Math.random()*11);
+            //  "numeroPoint":i
+            xi = myArgs[2];
+            yi = myArgs[3];
             var emit = {
-                "numeroPoint":i,
-                "PosRobotX": z,
-                "PosRobotY": a,
+                "PosRobotX": x,
+                "PosRobotY": y,
                 "Xi": xi,
                 "Yi": yi
             }
             client.emit('myevent', emit);
             console.log('galileo data send');    
-        }
+        //}
 
     }
-    setInterval(function(){
+Envoi();
+/*    setInterval(function(){
             Envoi();
-    } , 30000);
+    } , 10000);
+*/
+//Client.emit('disconnect');
+    // quit
+    //disconnect
 
