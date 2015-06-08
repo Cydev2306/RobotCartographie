905d430ccc2d4b6e306dd3165d86cdaa91f82fce
Charlier Cyril <charlier.cy@gmail.com>
2015-06-08 10:28:25 +0200

    envoi par 10

--- galileo.js
+++ galileo.js
@@ -1,21 +1,26 @@
 var clientio  = require('socket.io-client');
-var client    = clientio.connect('http://localhost:3013');
+var client    = clientio.connect('http://194.57.110.4:80');
 client.on('identifiant',function(data){
         console.log('Vous avez cet identifiant : '+ data);
 });
 client.emit('client', 'galileo');
 function Envoi() {
-         a =Math.floor(Math.random()*11) ,z=Math.floor(Math.random()*11), xi=Math.floor(Math.random()*11), yi =Math.floor(Math.random()*11);
-        var emit = {
-            "PosRobotX": z,
-            "PosRobotY": a,
-            "Xi": xi,
-            "Yi": yi
+         a =Math.floor(Math.random()*11) ,z=Math.floor(Math.random()*11)
+        for(var i=1;i<=10;i++){
+            xi=Math.floor(Math.random()*11), yi =Math.floor(Math.random()*11);
+            var emit = {
+                "numeroPoint":i,
+                "PosRobotX": z,
+                "PosRobotY": a,
+                "Xi": xi,
+                "Yi": yi
+            }
+            client.emit('myevent', emit);
+            console.log('galileo data send');    
         }
-        client.emit('myevent', emit);
-        console.log('galileo data send');
+
     }
     setInterval(function(){
             Envoi();
-    } , 2000);
+    } , 30000);
 
